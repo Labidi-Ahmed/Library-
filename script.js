f9f31fdfc1125c89a446f088fbb1160eaffb975e
Labidi-Ahmed <jackitunisi@gmail.com>
2024-01-15 18:30:01 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,131 @@
+const myLibrary = [];
+const appendedBooks = [];
+let indexOfDisplayedBook = 0;
+
+
+// BOOK CONSTRUCTOR 
+
+function book (title,author,pages,read){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+    this.info = ()=>{
+        
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+
+
+    }
+
+}
+
+
+
+function addBookToLibrary(book){
+    myLibrary.push(book);
+
+
+}
+
+// DISPLAY THE BOOK TO THE PAGE 
+
+function display(){
+    myLibrary.forEach((book)=>{
+        // CHECKS IF THE BOOK WAS ALREADY APPENDED 
+        const isAppended = appendedBooks.some((element)=>{
+            return element.title == book.title;
+        })
+        if(isAppended == false){
+            // appending  book to appended books 
+            appendedBooks.push(book);
+
+       
+
+            // SETS THE BOOK IN APPENDED BOOKS TO ENSURE THAT ALL THE BOOKS ARE UNIQUE 
+            
+
+       
+        // CREATING THE ELEMENTS OF THE BOOK
+        const container=document.createElement('div');
+        const title=document.createElement('div');
+        const author = document.createElement('div');
+        const pages = document.createElement('div');
+        const read=document.createElement('button');
+        const remove=document.createElement('button');
+        
+        // ADDING CLASSES TO THEM
+        container.classList.add('book-container');
+        title.classList.add("title");
+        author.classList.add("author");
+        pages.classList.add("pages")
+        read.classList.add("read");
+        remove.classList.add("remove");
+
+        // ADDING THE CONTENT TO THEM
+        title.textContent=book.title;
+        author.textContent=book.author;
+        pages.textContent=(`${book.pages} pages`);
+        let text="";
+        book.read?text="read":text="not read";
+        read.textContent=text;
+        remove.textContent="remove";
+        container.appendChild(title);
+        container.appendChild(author);
+        container.appendChild(pages);
+        container.appendChild(read);
+        container.appendChild(remove);
+        document.querySelector('.books-container').appendChild(container);
+    
+
+   
+    }
+
+
+
+
+        
+    })
+
+
+}
+
+
+
+
+
+
+const submit=document.querySelector(`button[type="submit"]`);
+const form=document.querySelector('form');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const erreurLabel=document.querySelector("#error");
+    erreurLabel.textContent="";
+    const title= document.querySelector('#title').value;
+    const author= document.querySelector('#author').value;
+    const pages= document.querySelector('#pages').value;
+    const read= document.querySelector('#read').checked;
+    const newBook= new book(title,author,pages,read);
+    const isAlreadyIncluded=myLibrary.some((book)=>{
+        return book.title==newBook.title;
+    })
+    if(isAlreadyIncluded == false){
+        addBookToLibrary(newBook);
+        display()
+        form.reset();
+    }
+    else{
+        
+        erreurLabel.textContent="THE BOOK ALREADY EXISTS";
+    }
+
+
+    
+    
+})
+
+
+const newBookButton=document.querySelector("#new-book");
+const modal=document.querySelector("#modal");
+newBookButton.addEventListener('click',() => {
+    modal.showModal();
+})
